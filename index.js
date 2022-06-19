d97e9fddb898c4c7015bc4a556ac9e7f90fa3835
leeric0512 <45275234+leeric0512@users.noreply.github.com>
2022-06-19 16:00:48 -0700

    Vanilla version
    Made to be as close to the original game as possible.

---
+++ index.js
@@ -0,0 +1,388 @@
+const options = [`do not press`,
+`*ahem*<br>do NOT press`,
+`...you pressed it again.`,
+`...you pressed it again.<br>Okay punk, press it again.`,
+`Yeah, that's it.<br>Come on, one more time.`,
+`Again.`,
+`Do it.`,
+`Okay, now you've had your fill.<br>Stop clicking.`,
+`You see, this is why we can't be friends.`,
+`You're just too selfish.`,
+`Grr... now you've asked for it.<br>Do NoT pReSs ThE bUtToN!`,
+`You seem to be immune to my mind control.`,
+`Time for Plan B.<br>Press it. You know you want to.`,
+`Mwahahahaha! Now let's see you press it!<br>LOSER!`,
+`...this displeases me.`,
+`HA! I have replaced the button with this penguin. Now what are you going to do?`,
+`...I hate you.`,
+`In that "cut off your head with a toothbrush" kind of way.`,
+`Quick! What's that behind you?!`,
+`Which one is it?<br>Not so smart now, are ya?`,
+`You're a clever one.<br>Time for Round 2.`,
+`You're a clever one.<br>Time for Round 2.`,
+`HAHAHAHAHAHA!`,
+`...`,
+`Okay, okay. You can press the button.<br>I don't care.`,
+`No really, I don't care anymore.`,
+`In fact, I lost interest a while ago.`,
+`I'm doing this just to entertain you.`,
+`Really.`,
+`Do you like cartoons?`,
+`...and paint?`,
+`Cartoons and Paint?`,
+`Well, you should.`,
+`Pick a colour.`,
+`Green. Perfect.`,
+`Press the red button.`,
+`Press the green button.`,
+`Press the blue button.`,
+`See, you just can't trust me.<br>Or can you?`,
+`You know, I'm glad we get to spend so much time together.`,
+`Doesn't it make you want to stop clicking big red buttons?`,
+`No seriously.`,
+`Look deep inside you.`,
+`Deeper.`,
+`DEEPER!`,
+`What if I told you that the next time you press the button, the world will explode?`,
+`See. You could have been dead right there.`,
+`And there.`,
+`You know, eventually I'll stop letting you get away with this.`,
+`The world is going to explode, and all you care about is pressing buttons.`,
+`Okay, this time the world will explode. I guarantee.`,
+`BOOM!<br>You're dead.`,
+`That wasn't very smart now was it?`,
+`Everyone's dead. Even you.`,
+`I'm not. I'm just text.`,
+`But you're dead.`,
+`Ha! Dead-face!`,
+`Stop clicking.`,
+`Have I ever told you how much I hate you?`,
+`Well I do.`,
+`I'm gonna start talking upside down if you click one more time.`,
+`HA! Can't read me now, can you?`,
+`What? You didn't want to read me anyway?`,
+`Well then, I'll show you!`,
+`&nbsp`,
+`&nbsp`,
+`You really are stubborn.`,
+`Stop clicking. Please.`,
+`See look. You've reduced me to begging. So please stop.`,
+`PLEASE!!!!`,
+`I'll give you a nickle.`,
+`Dime?`,
+`Quarter?`,
+`Aww come on! Just stop!`,
+`That does it! Time to unleash my master plan!`,
+`BEHOLD! The awesome power of CSS animation!`,
+`H8!`,
+`Let's get funky!`,
+`I'll bet you're starting to wonder why you've been doing this for so long.`,
+`Like jeez, all you've been doing is clicking a red button.`,
+`How lame is that?`,
+`But there is a bonus to all this.`,
+`But it's a secret.<br>So I can't tell you.`,
+`I got you going, didn't I?`,
+`You should've seen the look on your face! HA!`,
+`But seriously, there is a secret.<br>There's been one the whole time.`,
+`You've been busy clicking away at this big red button...`,
+`...when all the while a little white button has watched your every move.`,
+`MWAHAHAHAHAHAHA!`,
+`HAHAHA!`,
+`BWAHAA!`,
+`MWAAAAH!!`,
+`HAHA!`,
+`hehe!`,
+`lol`,
+`rofl`,
+`roflmao`,
+`roflmaoqxz`,
+`and so on and so forth`,
+`...`,
+`*whistles*`,
+`Find it yet?`,
+`Look harder!<br>It's right under your nose.`,
+`I know where it is.<br>But I'll never tell.`,
+`Or maybe I will...`,
+`But you gotta stop clicking the Big Red Button first!`,
+`Stop.`,
+`Now.`,
+`Fine, I won't tell you the secret.`,
+`Go ahead. Try to find it yourself.<br>You'll never find it.`,
+`Well you might... but the odds are against you.`,
+`~`,
+`What's your favourite letter?`,
+`Mine is the squiggly!`,
+`~`,
+`Find it yet?`,
+`No?`,
+`Then stop clicking and I'll tell you.`,
+`You know what?`,
+``,
+`Has anyone ever slapped you?`,
+`Cuz I will.`,
+`Really, I will.`,
+`It won't hurt though.`,
+`Cuz you're dead.`,
+`D-E-D`,
+`Remember? You went and blew up the entire planet!`,
+`Thought I'd forget about that, didn't you?`,
+`But an elephant never forgets!`,
+`...or something along those lines.`,
+`You killed everyone.`,
+`Sicko.`,
+`What would your mother say?`,
+`That's right... feel bad.`,
+`The world is null and you're to blame.`,
+`Not much you can do, since you're dead already.`,
+`So there's only one thing left that you can do...`,
+`Stop clicking the button.`,
+`Dude, you're dead.<br>What are you gaining from this?`,
+`Okay, everytime you click, you get sent to a lower layer of hell.`,
+`Welcome to layer 2.`,
+`3`,
+`4`,
+`5`,
+`6`,
+`7`,
+`8`,
+`9`,
+`BOOM!<br>You've exploded hell!`,
+`I hope you're happy.`,
+`Heaven's gone too.<br>That's how smooth you are.`,
+`Now you're dead, and there's no heaven or hell.`,
+`How does it feel?<br>You've condemned the world to limbo.`,
+`I once shot a man for being in limbo.`,
+`...or was he doing the limbo?`,
+`Meh. Tomato, tomahto.`,
+`This is getting boring, isn't it?`,
+`But you can't stop!`,
+`You want to end this.<br>You want to leave your computer.`,
+`But you can't!`,
+`You need to see if there's a pot of gold at the end of the rainbow!`,
+`But I've already told you how to find the pot of gold.`,
+`Follow the white rabbit.`,
+`..erm, button.`,
+`Remember the hidden button?`,
+`Or are you so self-centered that you forgot about that too?`,
+`I could just keep you here all day if I wanted.`,
+`You're in my world now.`,
+`No matter how much you hate it, you have to press the button.`,
+`again...`,
+`and again...`,
+`and again.`,
+`You just keep hoping something good will come of this.`,
+`Sure, I could tell you if I wanted to, but I'm not gonna.`,
+`You decided to keep clicking.<br>So I'm gonna enjoy it.`,
+`I mean, there's nothing else to enjoy.`,
+`You blew it all up, remember?`,
+`You're probably wondering who I am.`,
+`Well, let me tell you a tale about myself.`,
+`One day, I was walking home from work and I saw a small little house.`,
+`I knocked on the door out of curiosity, to see if anyone was home.`,
+`The door opened.`,
+`But no one was there.`,
+`I went inside to check it out and found nothing...`,
+`...nothing but a little metal box.`,
+`So I opened it.`,
+`Inside the box was a layer of foam protecting its contents.`,
+`I removed the foam and there it was...`,
+`...`,
+`... ...`,
+`ZZZzzzzzz`,
+`zzzzZZZZzzzz`,
+`ZZZZzzzZZZZ`,
+`Huh?`,
+`Oh! Right! The story!`,
+`So there I was, holding this little metal box in my hands.`,
+`The top was open and sitting inside was this...`,
+`Big Red Button.`,
+`And do you know what it said?`,
+`Oh I'll tell you what it said.<br>It said...`
+]
+
+
+const btnBase = document.querySelector("#btn-base");
+const text = document.querySelector("h1");
+const btnFace = document.querySelector("#btn-face");
+const btnDiv = document.querySelector("#btn-div");
+
+const decoyButton = `
+<button class="decoy-base">
+  <span class="decoy-face"></span>
+</button>`;
+const penguinImage = `<img src="./61207.png" alt="" id="penguin">`;
+
+
+let index = 0;
+
+text.innerHTML = options[0];
+index++;
+
+
+
+btnBase.addEventListener('click', () => {
+  text.innerHTML = options[index];
+  index++;
+
+  const decoyBases = document.querySelectorAll(".decoy-base");
+  const decoyFaces = document.querySelectorAll(".decoy-face");
+  const penguin = document.querySelector("#penguin");
+
+  decoyBases?.forEach(decoyBase => {
+    decoyBase.remove();
+  });
+  decoyFaces?.forEach(decoyFace => {
+    decoyFace.remove();
+  });
+
+  btnDiv.removeAttribute("class");
+  btnDiv.appendChild(btnBase, btnFace);
+  penguin?.remove();
+
+  document.querySelectorAll("*").forEach(change => {
+    change.style = null;
+  })
+
+
+  if(index === 14) {
+    btnFace.style.padding = "5px";
+    btnBase.style.backgroundColor = "transparent";
+  }
+
+  else if (index === 16) {
+    btnDiv.insertAdjacentHTML('afterbegin', penguinImage);
+    btnFace.style.backgroundColor = "transparent";
+    btnBase.style.backgroundColor = "transparent";
+  }
+
+  else if (index === 20) {
+    let decoys = 0;
+    while (decoys < 2) {
+      btnDiv.insertAdjacentHTML("afterbegin", decoyButton);
+      decoys++;
+    }
+  }
+
+   //make the real button a random placement?
+  else if (index === 22) {
+    let decoys = 0;
+    while (decoys < 18) {
+      btnDiv.insertAdjacentHTML("afterbegin", decoyButton);
+      decoys++;
+    }
+    while (decoys < 31 && decoys > 17) {
+      btnDiv.insertAdjacentHTML("beforeend", decoyButton);
+      decoys++;
+    }
+  }
+
+  else if (index === 23) {
+    let decoys = 0;
+    while (decoys < 34) {
+      btnDiv.insertAdjacentHTML("afterbegin", decoyButton);
+      decoys++;
+    }
+    while (decoys < 127 && decoys > 33) {
+      btnDiv.insertAdjacentHTML("beforeend", decoyButton);
+      decoys++;
+    }
+    const spans = document.querySelectorAll("span");
+    spans.forEach(span => {
+      span.style.padding = "25px"
+    })
+  }
+  
+  else if (index === 36) {
+    btnDiv.insertAdjacentHTML("afterbegin",
+    `<button class="decoy-base" style="background-color: #1d0b96;">
+      <span class="decoy-face" style="background-color: #2000f0;"></span>
+    </button>`);
+    btnDiv.insertAdjacentHTML("beforeend",
+    `<button class="decoy-base" style="background-color: #0b962b;">
+      <span class="decoy-face" style="background-color: #07de39;"></span>
+    </button>`);
+  }
+
+  else if (index === 37 || index === 38) {
+    btnDiv.insertAdjacentHTML("afterbegin",
+    `<button class="decoy-base" style="background-color: #1d0b96;">
+      <span class="decoy-face" style="background-color: #2000f0;"></span>
+    </button>
+    ${decoyButton}`);
+    btnBase.style.backgroundColor = "#0b962b";
+    btnFace.style.backgroundColor = "#07de39";
+  }   
+  
+  else if (index === 62 || index === 63 || index === 64) {
+    text.style.transform = "rotate(180deg)";
+  }
+
+  else if (index === 76) {
+    btnDiv.setAttribute("class", "animate-div");
+  }
+
+  else if (index === 78) {
+    btnBase.style.transform = "translateX(10rem) translateY(-15rem)";
+    btnFace.style.padding = "20px";
+    btnDiv.style.marginTop = "20%";
+
+    let decoys = 0;
+    while (decoys < 14) {
+      btnDiv.insertAdjacentHTML("beforeend",
+      `<button class="decoy-base" style = "position:absolute">
+        <span class="decoy-face"></span>
+      </button>`);
+      decoys++;
+    }
+    const decoyBases = document.querySelectorAll(".decoy-base");
+    const decoyFaces = document.querySelectorAll(".decoy-face");
+  
+    const btnPosition = [...decoyBases];
+    decoyBases.forEach(btn => { 
+      btnPosition[0].style.transform = `translate(-25rem, -15rem)`;
+      btnPosition[1].style.transform = `translate(-20rem, -10rem)`;
+      btnPosition[2].style.transform = `translate(-21rem, 4rem)`;
+      btnPosition[3].style.transform = `translate(-12rem)`;
+      btnPosition[4].style.transform = `translate(-7rem, -11rem)`;
+      btnPosition[5].style.transform = `translate(2rem, -15rem)`;
+      btnPosition[6].style.transform = `translate(1rem, -9rem)`;
+      btnPosition[7].style.transform = `translate(9rem, -8rem)`;
+      btnPosition[8].style.transform = `translate(20rem, -12rem)`;
+      btnPosition[9].style.transform = `translate(15rem, -3rem)`;
+      btnPosition[10].style.transform = `translate(25rem, -2rem)`;
+      btnPosition[11].style.transform = `translate(22rem, 5rem)`;
+      btnPosition[12].style.transform = `translate(12rem, 6rem)`;
+    })
+    const btnSize = [...decoyFaces];
+    btnSize.forEach(btn => {
+      btnSize[0].style.padding = "20px";
+      btnSize[1].style.padding = "80px";
+      btnSize[3].style.padding = "35px";
+      btnSize[4].style.padding = "55px";
+      btnSize[5].style.padding = "40px";
+      btnSize[6].style.padding = "25px";
+      btnSize[8].style.padding = "75px";
+      btnSize[9].style.padding = "30px";
+      btnSize[10].style.padding = "45px";
+      btnSize[11].style.padding = "35px";
+      btnSize[13].style.padding = "100px";
+    })
+  }
+
+  else if (index === 120) {
+    text.appendChild(btnBase, btnFace);
+    text.insertAdjacentHTML("afterbegin", `POOF! It's g`+`&#8202`);
+    text.insertAdjacentHTML("beforeend", `&#8202` + `ne!`)
+    btnFace.style.padding = "4px";
+    btnFace.style.margin = "-5px";
+    btnFace.style.border = "3px solid black";
+    btnBase.style.backgroundColor = "transparent";
+  }
+
+  else if(index === options.length) {
+    index = 0;
+    text.innerHTML = options[index];
+    index++;
+  }
+
+});
